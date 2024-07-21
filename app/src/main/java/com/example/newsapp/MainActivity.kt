75b1dd83e1f8f79dcbf2fb27134a47c33a84b95a
Erbol Mederbekov <erba522442@gmail.com>
2024-07-21 19:05:01 +0800

    Finished implementation of system ui controller and paging3

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.example.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.newsapp.domain.usecases.AppEntryUseCases
 import com.example.newsapp.presentation.navgraph.NavGraph
-import com.example.newsapp.presentation.onboarding.OnboardingScreen
-import com.example.newsapp.presentation.onboarding.components.OnboardingViewModel
 import com.example.newsapp.ui.theme.NewsAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -34,9 +30,16 @@ class MainActivity : ComponentActivity() {
                 viewModel.splashCondition
             }
         }
-
         setContent {
             NewsAppTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons =  !isSystemInDarkMode
+                    )
+                }
                 Box(
                     modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
                 ) {
